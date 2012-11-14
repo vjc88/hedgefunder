01b29f0be04cd4e1196fa934b989ce477141cf33
VJ Cerniglia <cernvj8@gmail.com>
2012-11-13 21:53:45 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hedgefunder::Application.config.secret_token = 'ab5b84ff9c4e67bd21b427ce2e7924d134bcb157c59bf0550fa773958532dd8d5762e6f98c830b3e4ceb27524d4de5b499946f71c5862f038db9ae8a55697776'
